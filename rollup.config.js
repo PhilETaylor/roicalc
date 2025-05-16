60bea06f708e13fe6c1c4494010b3dedf04ca8c1
Phil E. Taylor <phil@phil-taylor.com>
2025-05-16 09:42:18 +0100

    wip
    Signed-off-by: Phil E. Taylor <phil@phil-taylor.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
+import gzip from 'rollup-plugin-gzip';
 
 export default {
   input: 'src/roi-calculator-form.js',
@@ -10,7 +12,33 @@ export default {
   },
   plugins: [
     nodeResolve(),
-    terser() // Minify the bundle
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true
+    }),
+    terser({
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_getters: true,
+        passes: 2,
+        unsafe: true,
+        unsafe_comps: true,
+        unsafe_math: true,
+        unsafe_methods: true
+      },
+      mangle: {
+        properties: {
+          regex: /^_/  // Only mangle properties that start with underscore
+        }
+      },
+      format: {
+        comments: false
+      }
+    }),
+    gzip({
+      fileName: '.gz'
+    })
   ],
   preserveEntrySignatures: false
 };
